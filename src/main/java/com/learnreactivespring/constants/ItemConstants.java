eb98b30d1c4ec4b8e1a1933f66d4975d52bd2bef
Maciej Lotysz <maciek.lotysz@gmail.com>
2021-03-05 20:12:25 +0100

    Added JUnit tests for ItemController

--- src/main/java/com/learnreactivespring/constants/ItemConstants.java
+++ src/main/java/com/learnreactivespring/constants/ItemConstants.java
@@ -2,5 +2,5 @@ package com.learnreactivespring.constants;
 
 public class ItemConstants {
 
-    public static final String ITEM_ENDPOINT_V1 = "api/v1/items";
+    public static final String ITEM_ENDPOINT_V1 = "/api/v1/items";
 }
